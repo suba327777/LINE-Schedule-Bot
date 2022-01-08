a98bff4ab772520636d1fc22513c32a1889c1b14
suba327777 <suba.1357@ezweb.ne.jp>
2022-01-08 16:30:07 +0900

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,62 @@
+// Load the package
+import {
+  Client,
+  ClientConfig,
+  middleware,
+  MiddlewareConfig,
+  WebhookEvent,
+} from "@line/bot-sdk";
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+// Load the module
+import { SendMessage } from "./common/send/Sendmessage";
+
+const PORT = process.env.PORT || 3000;
+
+const config: ClientConfig = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || "",
+  channelSecret: process.env.CHANNEL_SECRET || "",
+};
+
+const middlewareConfig: MiddlewareConfig = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || "",
+  channelSecret: process.env.CHANNEL_SECRET || "",
+};
+
+// Create a new Express application.
+const app: express.Express = express();
+// Create a new LINE SDK client.
+const client = new Client(config);
+
+// Testing Routing
+app.get("/", (req: express.Request, res: express.Response): void => {
+  res.send("hello");
+});
+
+// App Routing
+app.post(
+  "/webhook",
+  middleware(middlewareConfig),
+  async (req: express.Request, res: express.Response): Promise<void> => {
+    // Respond to LINE side with status code 200 ahead of time.
+    res.sendStatus(200);
+
+    // Assign only the 0th element of the array from the events array to a variable.
+    const events: WebhookEvent[] = req.body.events;
+
+    events.map(async (event: WebhookEvent): Promise<void> => {
+      try {
+        await SendMessage(client, event);
+      } catch (e: unknown) {
+        console.log(e);
+      }
+    });
+  }
+);
+
+// Start the server
+app.listen(PORT, (): void => {
+  console.log(`Application is live and listening on port ${PORT}`);
+});
