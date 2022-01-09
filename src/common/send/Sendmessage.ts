b6ca74fb24b3f9282b2f0bd087c3ce093d289993
suba327777 <suba.1357@ezweb.ne.jp>
2022-01-09 13:54:45 +0900

    クイックリプライ機能

--- src/common/send/Sendmessage.ts
+++ src/common/send/Sendmessage.ts
@@ -3,13 +3,14 @@ import { Client, WebhookEvent } from "@line/bot-sdk";
 
 // Load the module
 import { ErrorMessage } from "../template/ErrorMessage";
+import { QuickReplyButton } from "../template/button/QuickReplyButton";
 
 export const SendMessage = async (
   client: Client,
   event: WebhookEvent
 ): Promise<void> => {
   try {
-    // すべてのイベントの処理
+    // メッセージ受信の場合とテキストメッセージの場合
     if (event.type !== "message" || event.message.type !== "text") {
       return;
     }
@@ -17,8 +18,9 @@ export const SendMessage = async (
     const { replyToken } = event;
     const { text } = event.message;
 
-    if (text === "予定教えて") {
-      // Reply p
+    if (text === "予定") {
+      // Reply
+      await client.replyMessage(replyToken, QuickReplyButton());
     } else {
       await client.replyMessage(replyToken, ErrorMessage());
     }
