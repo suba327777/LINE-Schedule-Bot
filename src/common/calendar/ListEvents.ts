158d51aa8162bfa1140a0c4bd4361f5e33772c54
suba327777 <suba.1357@ezweb.ne.jp>
2022-02-02 20:57:12 +0900

    予定を取得する期間の追加

--- src/common/calendar/ListEvents.ts
+++ src/common/calendar/ListEvents.ts
@@ -5,17 +5,37 @@ dotenv.config();
 
 // Load the module
 import { JwtClient } from "./JwtClient";
+import moment from "moment";
 
-export const ListEvents = async (): Promise<any> => {
+export const ListEvents = async (typeOfSchedule: string): Promise<any> => {
   try {
     const jwt = await JwtClient();
     const calendar: calendar_v3.Calendar = google.calendar("v3");
+
+    let startDate = moment(new Date());
+    let endDate = moment(new Date());
+
+    switch (typeOfSchedule) {
+      case "today":
+        endDate.endOf("days");
+        break;
+      case "tomorrow":
+        startDate.add(1, "days").startOf("days");
+        endDate.add(1, "days").endOf("days");
+        break;
+      case "nextWeek":
+        startDate.add(7, "days").day(1).startOf("days");
+        endDate.add(7, "days").day(7).endOf("days");
+        console.log(endDate);
+        break;
+    }
 
     // JWTの認証を行ってイベントのリストを表示する
     const calendars = await calendar.events.list({
       auth: jwt,
       calendarId: process.env.CALENDAR_ID,
-      timeMin: new Date().toISOString(),
+      timeMin: startDate.toISOString(),
+      timeMax: endDate.toISOString(),
       singleEvents: true,
       orderBy: "startTime",
     });
