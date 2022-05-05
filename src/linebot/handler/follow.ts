c0b3eff8094c17398edb2d1944afb8efac855e45
suba327777 <suba.1357@ezweb.ne.jp>
2022-05-05 21:50:00 +0900

    change file name

---
+++ src/linebot/handler/follow.ts
@@ -0,0 +1,10 @@
+/* packages */
+import { FollowEvent } from "@line/bot-sdk";
+/* lib */
+import { lineClient } from "../../lib/line";
+/* message */
+import { followMessage } from "../template/notice-messages/follow";
+
+export const followHandler = async (event: FollowEvent): Promise<void> => {
+  await lineClient.replyMessage(event.replyToken, followMessage);
+};
