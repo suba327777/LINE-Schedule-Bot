ab529843ec8f4bae8b84d77701fe1ed36a168483
suba <84484832+suba327777@users.noreply.github.com>
2023-01-27 16:53:13 +0900

    rename : folder name

---
+++ src/linebot/template/messages/listSchdule.ts
@@ -0,0 +1,39 @@
+/* packages */
+import { TextMessage } from "@line/bot-sdk";
+import moment from "moment";
+/* api */
+import { listEvents } from "../../api/googleCalendar/listEvents";
+/* types */
+import { listEventType } from "../../api/googleCalendar/types/listEventType";
+
+export const listSchduleMessage = async (typeOfSchedule: string): Promise<TextMessage> => {
+  // 予定の取得
+  const schdule: any = await listEvents(typeOfSchedule);
+
+  const data: listEventType = schdule;
+
+  let message = "";
+
+  if (data.length) {
+    data.map((res) => {
+      const start = moment(res.start?.dateTime);
+      // 日付のformat
+      const formatStart = start.format("MM月DD日HH時mm分 (ddd)");
+
+      const addMessage = `${formatStart}-${res.summary}`;
+
+      if (message === "") {
+        return (message = addMessage);
+      } else {
+        return (message += "\n\n" + addMessage);
+      }
+    });
+  } else {
+    message = "予定はないみたいですね";
+  }
+
+  return {
+    type: "text",
+    text: message,
+  };
+};
