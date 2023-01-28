d9365aada612095866708dd61b07bd8b7b00aae5
suba <84484832+suba327777@users.noreply.github.com>
2023-01-29 08:05:19 +0900

    update : db structure

--- src/constants/db.ts
+++ src/constants/db.ts
@@ -4,6 +4,6 @@ export const contextDB = new JsonDB(new Config("db/contextDB.json", true, true, 
 export const scheduleDB = new JsonDB(new Config("db/scheduleDB.json", true, true, "/"));
 
 export const resetDB = async () => {
-  await contextDB.push("/context", "");
-  await scheduleDB.push("/schedule", "");
+  await contextDB.push("/context", { name: "", date: "" });
+  await scheduleDB.push("/schedule", { name: "", date: "" });
 };
